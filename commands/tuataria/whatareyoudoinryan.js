d7f7b007b5a2b1aedcfb3a0c44506fe566a6c02b
AnythingGlenn <anythingglenn@yahoo.com>
2017-08-15 12:19:45 +0800

    Please add this. It is awesome
    I worked sooooo hard for this piece of art, so please add it

---
+++ commands/tuataria/whatareyoudoinryan.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class ShirtCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'whatareyoudoingryan',
+      aliases: ['whatareyoudoing', 'whatsupryan', 'supryan'],
+      group: 'tuataria',
+      memberName: 'whatareyoudoingryan',
+      description: 'See Ryan cry about being worse than Kyle.',
+      guildOnly: true
+    });
+  }
+var responses = [
+"Wishing I was as cool as Kyle", 
+"*wakes up* ..wha? KYLE WAS HERE? IS HE HERE NOW?! Oh ok back to sleep.",
+"One day, Some day, I will beeeeeee as cool as Kyle.",
+]
+  run(msg) {
+    if (msg.guild.name !== 'Tuataria') return msg.delete();
+    var random = Math.floor(Math.random() * responses.length);
+    msg.channel.send(responses[random]);
+  }
+};
