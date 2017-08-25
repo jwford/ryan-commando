1420ef761e297646757827c6d5b8b320c1ab53a1
jwford <jonathanwford156@gmail.com>
2017-08-25 16:46:45 -0400

    why not

---
+++ commands/tuataria/doyouwanttofight.js
@@ -0,0 +1,16 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class DoYouWantToFightCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'doyouwanttofight',
+      group: 'tuataria',
+      memberNmae: 'doyouwanttofight',
+      description: 'self explanatory'
+    });
+  }
+
+  run(msg) {
+    msg.channel.send('YES. I MUST BE THE ONLY RYAN.');
+  }
+};
