29215873c3df3ad2bf423b0d7c61dd255bddaee1
jwford <jonathanwford156@gmail.com>
2017-09-20 11:39:06 -0400

    reacts are now global

--- events/message.js
+++ events/message.js
@@ -5,11 +5,11 @@ module.exports = (oldMsg, newMsg) => {
   if (newMsg === undefined) {
     msg = oldMsg;
   } else msg = newMsg;
-  
+
   if (msg.channel.id === '342112503746265100') return; //stop auto-reacts in rinfo
 
   for (let phrase in reacts) {
-    var emojiRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+    let emojiRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
 
     let phraseRegex = new RegExp(phrase, 'i');
 
@@ -19,9 +19,16 @@ module.exports = (oldMsg, newMsg) => {
       if (emojiRegex.test(react)) {
         msg.react(react);
       } else {
-        react = msg.guild.emojis.find('name', react);
-        if (!react) return;
-        msg.react(react);
+        let ryanGuilds = msg.guild.members.get('345678639851110400').user.client.guilds;
+
+        for (let [id, guild] of ryanGuilds) { //eslint-disable-line no-unused-vars
+          if (guild.emojis.exists('name', react)) {
+            react = guild.emojis.find('name', react);
+            return msg.react(react);
+          }
+        }
+
+        return;
       }
     }
   }
