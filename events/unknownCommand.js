87dcc46db6ddcc3b5f7f15d1c1e56c674377d937
jwford <jonathanwford156@gmail.com>
2017-09-28 07:10:43 -0400

    exclude DMs and group DMs from unknown command squid react

--- events/unknownCommand.js
+++ events/unknownCommand.js
@@ -1,4 +1,5 @@
 module.exports = msg => {
+  if (msg.channel.type !== 'text') return;
   let squid = msg.guild.members.get('297890742771253258').user.client.guilds.get('273689397675687940').emojis.get('276372748534677504');
   if (!squid) return;
   msg.react(squid);
