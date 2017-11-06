9cfde3bbbb8fde157e5d491b9bee711406fb2021
Dominic Brady <dombrady1993@gmail.com>
2017-11-06 19:04:55 +0000

    Sideservers Improvement

--- commands/info_useful/sideservers.js
+++ commands/info_useful/sideservers.js
@@ -10,12 +10,34 @@ module.exports = class SideServersCommand extends Command {
       memberName: 'sideservers',
       description: 'Lists Tuataria side servers, with their invite links.',
       details: 'The links that this command provides don\'t work on iOS at this time. #BlameDiscord.'
+      args: [
+        {
+          key: 'sideserver',
+          prompt: 'Which sideserver would you like a link to?',
+          type: 'string',
+          default: ''
+        }
+      ]
     });
   }
 
   run(msg) {
-    let serverList = ['[Codetaria](https://discord.gg/yFKMrvZ)', '[Gamataria](https://discord.gg/8uTmbuX)', '[Hogwartaria](https://discord.gg/kVRp4Q7)', '[Tuataria Book Club](https://discord.gg/f5XvUgd)', '[Tuatariglot](https://discord.gg/cMZ5yWN)', '[Typotaria](https://discord.gg/QhZaK8a)', '[100 days](https://discord.gg/Jh4JMVn)'];
-
+    let servers = {
+      'Codetaria': 'https://discord.gg/yFKMrvZ',
+      'Gamataria': 'https://discord.gg/8uTmbuX',
+      'Hogwartaria': 'https://discord.gg/kVRp4Q7',
+      'Tuataria Book Club': 'https://discord.gg/f5XvUgd',
+      'Tuatariglot': 'https://discord.gg/cMZ5yWN',
+      'Typotaria': 'https://discord.gg/QhZaK8a',
+      '100 days': 'https://discord.gg/Jh4JMVn'
+    };
+    if (args.sideserver) {
+      return msg.reply(args.sideserver in servers ? servers[args.sideserver] : 'I couldn\'t find that server');
+    }
+    let serverList = [];
+    for (server in servers) {
+      serverList.push(`[${server}](${servers[server]})`);
+    }
     msg.channel.send(new RichEmbed()
     .setColor(0x276df9)
     .addField('Sideservers', serverList, true));
